427d863d537036b7df4965031ae1fc8a1706f87b
Mensah Michael <msquaremega@gmail.com>
2022-01-28 06:17:23 +0100

    some designs in progress

--- src/app/components/profile/home/home.component.ts
+++ src/app/components/profile/home/home.component.ts
@@ -1,6 +1,8 @@
 import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { gsap } from 'gsap';
+import { CSSRulePlugin } from 'gsap/all';
 import { GUI } from 'dat.gui';
 
 @Component({
@@ -21,7 +23,7 @@ export class HomeComponent implements OnInit, AfterViewInit  {
   public camera!: THREE.PerspectiveCamera;
   private orbitControl!: OrbitControls;
 
-  // private gui = new GUI;
+  // private gui = new GUI({autoPlace: true});
 
   // Texture Loader
   public textureLoader = new THREE.TextureLoader();
@@ -87,15 +89,37 @@ export class HomeComponent implements OnInit, AfterViewInit  {
 
 
 
-  constructor() { }
+  constructor( ) { }
 
   ngOnInit(): void {
   }
 
 
   initial() {
+
+    gsap.registerPlugin(CSSRulePlugin);
+    const borderline = CSSRulePlugin.getRule('.content::before');
+    const h1 = document.querySelector('h1');
+    const p = document.querySelector('p');
+
+
+
+    const tl = gsap.timeline();
+    console.log(borderline);
+
+    tl.from(borderline, {cssRule: {scaleX: 0}});
+    tl.to(h1, {  clipPath: 'polygon(0 0, 100% 0, 100% 0%, 0% 100%)', y: '30px' }, "-=3");
+    tl.to(p, { duration: 2, clipPath: 'polygon(0 0, 100% 0, 100% 0%, 0% 100%)', y: '30px' }, "-=2");
+    // console.log(tl);
+
+    // tl.from(borderline, {delay: .5, duration: 4, });
+    // this.gui.domElement.id = 'gui';
+    // this.gui.domElement.style.position = 'absolutte';
+    // this.gui.domElement.style.top = '2px';
+    // this.gui.domElement.style.left ='2px';
     this.camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-    this.camera.position.setZ(30);
+    // this.camera.position.setZ(30);
+    this.camera.position.z = 30;
 
     this.scene = new THREE.Scene();
     this.scene.fog = new THREE.FogExp2(this.fogHex, this.fogDensity)
@@ -108,6 +132,10 @@ export class HomeComponent implements OnInit, AfterViewInit  {
     this.renderer.render(this.scene, this.camera);
 
     this.createParticles();
+
+    // this.guiControl();
+
+    this.particlesMesh.position.z = 31.5;
 
     this.scene.add(this.particlesMesh)
 
@@ -140,7 +168,6 @@ export class HomeComponent implements OnInit, AfterViewInit  {
       this.color.setRGB( vx, vy, vz );
 
       this.colors.push( this.color.r, this.color.g, this.color.b );
-      // console.log(this.colors);
     }
     this.particleGeometry.setAttribute('position', new THREE.BufferAttribute(this.particlePosition, 3));
     this.particleGeometry.setAttribute('color', new THREE.Float32BufferAttribute(this.colors, 3));
@@ -172,27 +199,38 @@ export class HomeComponent implements OnInit, AfterViewInit  {
     }
   }
 
+  // guiControl() {
+  //   const particlesMesh = this.gui.addFolder('Particles Control');
+
+  //   particlesMesh.add(this.particlesMesh.position, 'z').min(1).max(100).step(0.1);
+  // }
+
 
   onTouchStartParticle = (e: any) => {
-    if (e.isTrusted) {
-      if (e.touches.length === 1) {
-        e.preventDefault();
-        this.mouseX = e.touches[0].pageX - (window.innerWidth/2);
-        this.mouseY = e.touches[0].pageY - (window.innerHeight/2);
-      }
+    if (e !== undefined) {
+      if (e.isTrusted) {
+        if (e.touches.length === 1) {
+          e.preventDefault();
+          this.mouseX = e.touches[0].pageX - (window.innerWidth/2);
+          this.mouseY = e.touches[0].pageY - (window.innerHeight/2);
+          // e.defaultPrevented
+        }
 
+      }
     }
   }
 
   onTouchMoveParticles = (e: any) => {
-    if (e.isTrusted) {
-      if (e.touches.length === 1) {
-        e.preventDefault();
-        this.mouseX = e.touches[0].pageX - (window.innerWidth/2);
-        this.mouseY = e.touches[0].pageY - (window.innerHeight/2);
-        // e.defaultPrevented
-      }
+    if (e !== undefined) {
+      if (e.isTrusted) {
+        if (e.touches.length === 1) {
+          e.preventDefault();
+          this.mouseX = e.touches[0].pageX - (window.innerWidth/2);
+          this.mouseY = e.touches[0].pageY - (window.innerHeight/2);
+          // e.defaultPrevented
+        }
 
+      }
     }
   }
 
@@ -204,9 +242,9 @@ export class HomeComponent implements OnInit, AfterViewInit  {
     (function render() {
       requestAnimationFrame(render);
 
-      component.particlesMesh.rotation.x = -.1 * elapsedTime;
-      component.particlesMesh.rotation.y = -.005 * elapsedTime;
-      component.particlesMesh.rotation.z = -.1 * elapsedTime;
+      component.particlesMesh.rotation.x += -.01 * elapsedTime;
+      component.particlesMesh.rotation.y += -.0005 * elapsedTime;
+      component.particlesMesh.rotation.z += -.01 * elapsedTime;
 
       component.renderer.render(component.scene, component.camera);
     } ())
